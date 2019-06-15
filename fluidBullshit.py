16f7f713819e96926a9ddb5b391712f0542ae0cf
Kevin Winkelman <kevwinkelman@gmail.com>
2019-06-15 13:34:10 -0700

    Init
    Init

---
+++ fluidBullshit.py
@@ -0,0 +1,12 @@
+
+
+def getInputs():
+    m_w = input("Input m_W: ")
+    p = input("Input p (or Density): ")
+    k_f = input("Input k_f: ")
+    v = input("Input v: ")
+    m_c = input("Input m_W: ")
+
+getInputs()
+
+
